5f30d19a0381e5b26edd0f32f6fa4c0d0826c11e
zeynepsldrk <162623674+zeynepsldrk@users.noreply.github.com>
2025-05-24 00:00:52 +0300

    commit denemesi

---
+++ footballplayer/FootballPlayer.java
@@ -0,0 +1,16 @@
+public class FootballPlayer {
+
+    public String name;
+    public int minutes;
+    public boolean inPlay;
+    public int numberOfGoals;
+    public int no;
+
+    public void play(int minutesToPlay) {
+        minutes = minutes + minutesToPlay;
+    }
+    public void score() {
+        numberOfGoals = numberOfGoals + 1;
+    }
+
+}
---
+++ footballplayer/FootballPlayerTest.java
@@ -0,0 +1,18 @@
+public class FootballPlayerTest {
+    public static void main(String[] args) {
+
+        FootballPlayer alex = new FootballPlayer();
+        alex.no = 10;
+        alex.name = "Alex De Souza";
+        alex.minutes = 0;
+        alex.inPlay = true;
+        alex.numberOfGoals = 0;
+
+        alex.play(90);
+        alex.score();
+        alex.score();
+
+        System.out.println("Number of goals Alex scored: " + alex.numberOfGoals);
+        System.out.println("Alex played totally " + alex.minutes + " minutes so far.");
+    }
+}
