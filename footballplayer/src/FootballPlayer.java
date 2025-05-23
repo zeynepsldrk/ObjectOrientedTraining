274be9e9b6c48f868ff2c2dfe892b5190978dac6
zeynepsldrk <162623674+zeynepsldrk@users.noreply.github.com>
2025-05-23 23:05:49 +0300

    commitlendi

--- footballplayer/src/FootballPlayer.java
+++ footballplayer/src/FootballPlayer.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+public class FootballPlayer {
 
-public class footballplayer {
+    public String name;
+    public int minutes;
+    public boolean inPlay;
+    public int numberOfGoals;
+    public int no;
+
+    public void play(int minutesToPlay) {
+        minutes = minutes + minutesToPlay;
+    }
+    public void score() {
+        numberOfGoals = numberOfGoals + 1;
+    }
 }
