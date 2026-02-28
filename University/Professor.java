8ec4d037db9479a9e995c59a48a4826e06d24a07
zeynepsldrk <162623674+zeynepsldrk@users.noreply.github.com>
2026-02-28 18:17:43 +0300

    added setter getter methods

--- University/Professor.java
+++ University/Professor.java
@@ -6,4 +6,60 @@ public class Professor {
     Department department;
     Student[] advisee;
     Course[] coursesGiven;
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Student[] getAdvisee() {
+        return advisee;
+    }
+
+    public void setAdvisee(Student[] advisee) {
+        this.advisee = advisee;
+    }
+
+    public Course[] getCoursesGiven() {
+        return coursesGiven;
+    }
+
+    public void setCoursesGiven(Course[] coursesGiven) {
+        this.coursesGiven = coursesGiven;
+    }
 }
--- University/Student.java
+++ University/Student.java
@@ -5,4 +5,52 @@ public class Student {
     Department department;
     Professor advisor;
     Course[] coursesTaken;
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Professor getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(Professor advisor) {
+        this.advisor = advisor;
+    }
+
+    public Course[] getCoursesTaken() {
+        return coursesTaken;
+    }
+
+    public void setCoursesTaken(Course[] coursesTaken) {
+        this.coursesTaken = coursesTaken;
+    }
 }
