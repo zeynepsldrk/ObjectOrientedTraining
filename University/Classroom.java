db4e2c84ae14e19314c1aa9d0a9654510c09a1c9
zeynepsldrk <162623674+zeynepsldrk@users.noreply.github.com>
2026-02-28 18:19:15 +0300

    Created new relations between classrom and course. Added new student and make some exercises

---
+++ University/Classroom.java
@@ -0,0 +1,13 @@
+/*course içine bir instants varib. ekleyeceğiz classroom tipinde derslerin hangi sınıflarda */
+
+public class Classroom {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
