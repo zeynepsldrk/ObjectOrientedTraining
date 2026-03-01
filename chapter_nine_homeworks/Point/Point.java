e91e04c8f1d029925742e8b55ff0377e41168aee
zeynepsldrk <162623674+zeynepsldrk@users.noreply.github.com>
2026-03-01 17:57:38 +0300

    Started new execise about operation on point

---
+++ chapter_nine_homeworks/Point/Point.java
@@ -0,0 +1,43 @@
+package Point;
+
+public class Point {
+
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /*point in orjin e uzaklığını ve başka bir pointe uzaklığını hesaplayan iki metot
+    * aynı point i klonlayan başka bir metot
+    * move metodunda x veya y yönünde hareket edecek
+    * iki tane point nesnesi ile denemeler yap*/
+
+    public int calculateDistanceOfOrigin()
+    {
+        return ();
+    }
+
+    public int calculateDistanceOfAnotherPoint(Point point)
+    {
+
+    }
+
+    public Point movePoint(int x, int y)
+    {
+
+    }
+}
