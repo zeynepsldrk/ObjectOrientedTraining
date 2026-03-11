9f6423a59d9ee4e204179bcecaacc772e74ef11c
zeynepsldrk <162623674+zeynepsldrk@users.noreply.github.com>
2026-03-11 16:43:11 +0300

    that codes is already in my course source code.

---
+++ chapter_nine_homeworks/StatefulCalculator/StatefulCalculator.java
@@ -0,0 +1,84 @@
+package StatefulCalculator;
+
+public class StatefulCalculator {
+
+    String name;
+
+    int firstArgument;
+    int secondArgument;
+    double result;
+    char operation;
+
+    // That's the engine that does calculations.
+    Calculator calculator = new Calculator();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    void whoAreYou() {
+        System.out.println("I'm a Calculator, my name is " + name);
+    }
+
+    public int getFirstArgument() {
+        return firstArgument;
+    }
+
+    public void setFirstArgument(int newFirstArgument) {
+        firstArgument = newFirstArgument;
+    }
+
+    public int getSecondArgument() {
+        return secondArgument;
+    }
+
+    public void setSecondArgument(int newSecondArgument) {
+        secondArgument = newSecondArgument;
+    }
+
+    public char getOperation() {
+        return operation;
+    }
+
+    public void setOperation(char newOperation) {
+        operation = newOperation;
+    }
+
+    public double getResult() {
+        switch(operation){
+            case '+':	result = calculator.add(firstArgument, secondArgument); break;
+            case '-':	result = calculator.subtract(firstArgument, secondArgument); break;
+            case '*':	result = calculator.multiply(firstArgument, secondArgument); break;
+            case '/':	result = calculator.divide(firstArgument, secondArgument); break;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        StatefulCalculator calculator = new StatefulCalculator();
+
+        int a = 12;
+        int b = 27;
+
+        calculator.setFirstArgument(a);
+        calculator.setSecondArgument(b);
+        calculator.setOperation('*');
+        double result = calculator.getResult();
+
+        System.out.println("Result: " + result);
+        System.out.println(calculator.getFirstArgument() + " " + calculator.getOperation() + " " + calculator.getSecondArgument() + " = " + result);
+
+        calculator.setFirstArgument(17);
+        calculator.setSecondArgument(92);
+        calculator.setOperation('/');
+        result = calculator.getResult();
+        System.out.println("Result: " + result);
+        System.out.println(calculator.getFirstArgument() + " " + calculator.getOperation() + " " + calculator.getSecondArgument() + " = " + result);
+    }
+}
+
